ddfdb9e86c87179322db43b78f691dd0b466b288
Anatoliy Bernatovich <raixur@gmail.com>
2018-02-06 19:13:17 +0200

    Replace old game instances with new SteamGameInstance
    * Update Server Menu.
    * Add server search prototype.
    * Add server list update.
    * Remove BP_GameInfoInstance
    * Remove SteamMultiplayerGameInstance

--- Source/AureumVitae/AureumVitae.Build.cs
+++ Source/AureumVitae/AureumVitae.Build.cs
@@ -14,7 +14,7 @@ public class AureumVitae : ModuleRules
 
 	    PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "OnlineSubsystem", "OnlineSubsystemUtils" });
 
-	    DynamicallyLoadedModuleNames.Add("OnlineSubsystemNull");
+	    DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
         // Uncomment if you are using Slate UI
         // PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
